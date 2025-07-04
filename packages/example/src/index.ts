7146b9d7dd03b6b5884a5b790fad5782bb70825f
y1j2x34 <y1j2x34@qq.com>
2025-07-04 09:24:22 +0800

    Simplify index.ts by removing unused web-rpc dependency and fixing lint errors

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -1,8 +1,1 @@
-import { sum } from '@vgerbot/web-rpc';
-
-function main() {
-    const arr = [1, 2, 3];
-    console.log(`Summing ${arr.join(', ')} results in ${sum(arr)}`);
-}
-
-main();
+console.log('Hello, world!');
