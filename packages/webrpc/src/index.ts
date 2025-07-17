8b57a6a99c8b64a7601d4760e4c57172e4255cec
y1j2x34 <y1j2x34@qq.com>
2025-07-17 22:26:27 +0800

    feat: add CDN installation support and restructure README documentation
    - Symlink root README to package-specific documentation
    - Add CDN installation instructions for ES modules and IIFE builds
    - Configure package.json for browser/CDN distribution
    - Enhance build config with IIFE format for global browser usage

--- packages/webrpc/src/index.ts
+++ packages/webrpc/src/index.ts
@@ -2,3 +2,10 @@ export * from './core/WebRPC';
 export * from './transports';
 export * from './core/Transport';
 export * from './core/SendFunctionTransport';
+
+import { WebRPC } from './core/WebRPC';
+import * as transports from './transports';
+
+Object.assign(WebRPC, transports, { WebRPC });
+
+export default WebRPC;
